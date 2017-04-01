111a3204798c32e8edc69203ddc150de70b34cdd
Jose Garcia <jose.garcia12@upr.edu>
2017-04-01 04:49:48 -0400

    Question 1 solution

--- mapreduce/p1exam1/src/main/java/edu/uprm/cse/bigdata/p1exam1/App.java
+++ mapreduce/p1exam1/src/main/java/edu/uprm/cse/bigdata/p1exam1/App.java
@@ -1,5 +1,12 @@
 package edu.uprm.cse.bigdata.p1exam1;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+      try {
+            Job job = new Job();
+            job.setJarByClass(edu.uprm.cse.bigdata.p1exam1.App.class);
+            job.setJobName("Count Occurrences of Specific Words");
+
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+            job.setMapperClass(edu.uprm.cse.bigdata.p1exam1.Mapper.class);
+            job.setReducerClass(edu.uprm.cse.bigdata.p1exam1.Reducer.class);
+
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(Text.class);
+
+            return job.waitForCompletion(true);
+        }
+        catch(Exception ex)
+        {
+            System.out.println(ex.getMessage());
+            return false;
+        }
     }
 }
